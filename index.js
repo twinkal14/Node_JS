a3b2710010ae4df49c9936ebd41ce36dba9aa5d8
twinkal14 <127021613+twinkal14@users.noreply.github.com>
2024-04-17 23:55:43 +0530

    Add files via upload
    It's an simple arithmetic calculation in function format at node_js.

---
+++ index.js
@@ -0,0 +1,7 @@
+const calculator1=require('./calculator')
+
+let x=50,y=20;
+console.log("Addition =",calculator1.add(x,y));
+console.log("Substraction =",calculator1.sub(x,y));
+console.log("Multiplication =",calculator1.multi(x,y));
+console.log("Division =",calculator1.div(x,y));
